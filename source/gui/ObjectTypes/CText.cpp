YavorLazarovTUES/0ad-GL2026

commit 8a2a4506866c25c2935225950f154a901c536f1e

Align text as usual if the scrollbar is invisible
Previously, while `scrollbar` was set to true, the text was always
vertically aligned to the top, no matter what its `text_valign` was, by
the scrolling logic. However, this was done even when the text's caption
was so short that no scrollbar was required in the first place (and not
rendered). Falling back to the specified `text_valign` value in that case
instead seems like the expected behavior.

On a few occasions in the GUI, the text was supposed to be aligned to
the top in either case, but still set `text_valign` to a different value
(for whatever reason), which didn't have any effect previously. But
now since it does, the values have to be corrected to specify what is
actually desired.

source/gui/ObjectTypes/CText.cpp
@@ -1,4 +1,4 @@
-/* Copyright (C) 2025 Wildfire Games.
+/* Copyright (C) 2026 Wildfire Games.
  * This file is part of 0 A.D.
  *
  * 0 A.D. is free software: you can redistribute it and/or modify
@@ -71,9 +71,6 @@ void CText::SetupText()
 
 	m_GeneratedTexts[0] = CGUIText(m_pGUI, m_Caption, m_Font, width, m_BufferZone, m_TextAlign, this);
 
-	if (!m_ScrollBar)
-		CalculateTextPosition(m_CachedActualSize, m_TextPos, m_GeneratedTexts[0]);
-
 	// Setup scrollbar
 	if (m_ScrollBar)
 	{
@@ -99,6 +96,9 @@ void CText::SetupText()
 		if (m_ScrollTop)
 			GetScrollBar(0).SetPos(0.0f);
 	}
+
+	if (!m_ScrollBar || !std::ranges::any_of(m_ScrollBars, &IGUIScrollBar::IsVisible))
+		CalculateTextPosition(m_CachedActualSize, m_TextPos, m_GeneratedTexts[0]);
 }
 
 void CText::ResetStates()
@@ -204,14 +204,14 @@ void CText::Draw(CCanvas2D& canvas)
 
 	const CGUIColor& color = m_Enabled ? m_TextColor : m_TextColorDisabled;
 
-	if (m_ScrollBar)
+	if (m_ScrollBar && std::ranges::any_of(m_ScrollBars, &IGUIScrollBar::IsVisible))
+	{
 		DrawText(canvas, 0, color, m_CachedActualSize.TopLeft() - CVector2D(0.f, scroll), cliparea);
+		// Draw scrollbars on top of the content
+		IGUIScrollBarOwner::Draw(canvas);
+	}
 	else
 		DrawText(canvas, 0, color, m_TextPos, cliparea);
-
-	// Draw scrollbars on top of the content
-	if (m_ScrollBar)
-		IGUIScrollBarOwner::Draw(canvas);
 
 	// Draw the overlays last
 	m_pGUI.DrawSprite(m_SpriteOverlay, canvas, m_CachedActualSize, m_VisibleArea);

binaries/data/mods/mod/gui/common/modern/styles.xml
@@ -131,7 +131,7 @@
 		scrollbar_style="ModernScrollBar"
 		textcolor="white"
 		text_align="left"
-		text_valign="center"
+		text_valign="top"
 		sprite="ModernDarkBox"
 		sprite_overlay="ModernDarkBoxGoldBorder"
 	/>

binaries/data/mods/mod/gui/modmod/styles.xml
@@ -9,6 +9,6 @@
 		scroll_bottom="true"
 		textcolor="white"
 		text_align="left"
-		text_valign="center"
+		text_valign="top"
 	/>
 </styles>

binaries/data/mods/public/gui/common/styles.xml
@@ -135,7 +135,7 @@
 		scroll_bottom="true"
 		textcolor="white"
 		text_align="left"
-		text_valign="center"
+		text_valign="top"
 		sprite="ModernDarkBox"
 		sprite_overlay="ModernDarkBoxGoldBorder"
 	/>

JamesMarcil/0ad

commit 931c132b1b03b6faf8acd3bddd1f962e2d1d4ef5

Fix brief text misalignment in GUI text objects
The issue was introduced by 8a2a450686.
It affected GUI text objects (CText) with `scrollbar` set to true; when
their caption was long enough for a scrollbar to be visible and then the
size changed (got bigger) so that no scrollbar was needed anymore, then
for a single frame the text was completely misaligned. This happened
because in `CText::Draw` the scrollbar wasn't updated yet, so the newly
added `GetScrollBar(0).IsVisible()` check still returned true, even
though it wouldn't end up being rendered anyway -- it was updated in
`IGUITextOwner::DrawText` eventually.

This happened to the tutorial info panel in the starting economy
walkthrough, for example. Note: in that case the scrollbar wasn't ever
drawn, but it was temporarily set visible within a frame by the JS
object resizing logic.

source/gui/ObjectTypes/CText.cpp
@@ -181,6 +181,9 @@ void CText::Draw(CCanvas2D& canvas)
 {
 	m_pGUI.DrawSprite(m_Sprite, canvas, GetActualSize(), m_VisibleArea);
 
+	// Ensure the scrollbars are up-to-date.
+	UpdateText();
+
 	float scroll = 0.f;
 	if (m_ScrollBar)
 		scroll = GetScrollBar(0).GetPos();