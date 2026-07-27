8918cfb0a7e032dfe5006d856e9626a54af6aaa1
phosit <phosit@autistici.org>
2026-07-27 12:23:43 +0200

    Make incompatible_mods.js a module
    Refs: #8081

--- binaries/data/mods/mod/gui/incompatible_mods/incompatible_mods.js
+++ binaries/data/mods/mod/gui/incompatible_mods/incompatible_mods.js
@@ -1,6 +1,8 @@
-var g_IncompatibleModsFile = "gui/incompatible_mods/incompatible_mods.txt";
+/* eslint-disable prefer-const -- Mods should be able to change it */
+let g_IncompatibleModsFile = "gui/incompatible_mods/incompatible_mods.txt";
+/* eslint-enable prefer-const */
 
-function init(data)
+export function init(data)
 {
 	Engine.GetGUIObjectByName("mainText").caption = Engine.TranslateLines(Engine.ReadFile(g_IncompatibleModsFile));
 	return new Promise(closePageCallback =>
