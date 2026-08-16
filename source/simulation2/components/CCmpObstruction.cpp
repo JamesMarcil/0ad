JamesMarcil/0ad

commit 9ae084519f4a068b656d28ee575a44e618b255b8

Fix most of the new vs2017 induced warnings.
Refs: https://code.wildfiregames.com/D3096
https://code.wildfiregames.com/D3103 #5862
Reviewed by: @wraitii
Comments by: @Angen
Differential Revision: https://code.wildfiregames.com/D3126
This was SVN commit r24268.

source/simulation2/components/CCmpObstruction.cpp
@@ -771,18 +771,15 @@ public:
 				return;
 
 			// Attempt to replace colliding entities' control groups with a persistent one.
-			for (std::vector<entity_id_t>::iterator it = normalEnts.begin(); it != normalEnts.end(); ++it)
+			for (const entity_id_t normalEnt : normalEnts)
 			{
-				entity_id_t ent = *it;
-
-				CmpPtr<ICmpObstruction> cmpObstruction(GetSimContext(), ent);
-				for (std::vector<entity_id_t>::iterator it = persistentEnts.begin(); it != persistentEnts.end(); ++it)
+				CmpPtr<ICmpObstruction> cmpObstruction(GetSimContext(), normalEnt);
+				for (const entity_id_t persistent : normalEnts)
 				{
-					entity_id_t persistent = *it;
 					entity_id_t group = cmpObstruction->GetControlGroup();
 
 					// Only clobber 'default' control groups.
-					if (group == ent)
+					if (group == normalEnt)
 						cmpObstruction->SetControlGroup(persistent);
 					else if (cmpObstruction->GetControlGroup2() == INVALID_ENTITY && group != persistent)
 						cmpObstruction->SetControlGroup2(persistent);

source/graphics/MapReader.cpp
@@ -55,6 +55,10 @@
 
 #include <boost/algorithm/string/predicate.hpp>
 
+#if defined(_MSC_VER) && _MSC_VER > 1900
+#pragma warning(disable: 4456) // Declaration hides previous local declaration.
+#pragma warning(disable: 4458) // Declaration hides class member.
+#endif
 
 CMapReader::CMapReader()
 	: xml_reader(0), m_PatchesPerSide(0), m_MapGen(0)
@@ -421,7 +425,9 @@ private:
 	int el_garrison;
 	int el_turrets;
 	int el_actor;
-	int at_x, at_y, at_z;
+	int at_x;
+	int at_y;
+	int at_z;
 	int at_group, at_group2;
 	int at_angle;
 	int at_uid;
@@ -656,20 +662,20 @@ void CXMLReader::ReadEnvironment(XMBElement parent)
 		{
 			XERO_ITER_EL(element, fog)
 			{
-				int element_name = fog.GetNodeName();
-				if (element_name == el_fogcolor)
+				int fog_element_name = fog.GetNodeName();
+				if (fog_element_name == el_fogcolor)
 				{
-					XMBAttributeList attrs = fog.GetAttributes();
+					XMBAttributeList fogAttributes = fog.GetAttributes();
 					m_MapReader.m_LightEnv.m_FogColor = RGBColor(
-						attrs.GetNamedItem(at_r).ToFloat(),
-						attrs.GetNamedItem(at_g).ToFloat(),
-						attrs.GetNamedItem(at_b).ToFloat());
+						fogAttributes.GetNamedItem(at_r).ToFloat(),
+						fogAttributes.GetNamedItem(at_g).ToFloat(),
+						fogAttributes.GetNamedItem(at_b).ToFloat());
 				}
-				else if (element_name == el_fogfactor)
+				else if (fog_element_name == el_fogfactor)
 				{
 					m_MapReader.m_LightEnv.m_FogFactor = fog.GetText().ToFloat();
 				}
-				else if (element_name == el_fogthickness)
+				else if (fog_element_name == el_fogthickness)
 				{
 					m_MapReader.m_LightEnv.m_FogMax = fog.GetText().ToFloat();
 				}
@@ -679,24 +685,24 @@ void CXMLReader::ReadEnvironment(XMBElement parent)
 		{
 			XERO_ITER_EL(element, postproc)
 			{
-				int element_name = postproc.GetNodeName();
-				if (element_name == el_brightness)
+				int post_element_name = postproc.GetNodeName();
+				if (post_element_name == el_brightness)
 				{
 					m_MapReader.m_LightEnv.m_Brightness = postproc.GetText().ToFloat();
 				}
-				else if (element_name == el_contrast)
+				else if (post_element_name == el_contrast)
 				{
 					m_MapReader.m_LightEnv.m_Contrast = postproc.GetText().ToFloat();
 				}
-				else if (element_name == el_saturation)
+				else if (post_element_name == el_saturation)
 				{
 					m_MapReader.m_LightEnv.m_Saturation = postproc.GetText().ToFloat();
 				}
-				else if (element_name == el_bloom)
+				else if (post_element_name == el_bloom)
 				{
 					m_MapReader.m_LightEnv.m_Bloom = postproc.GetText().ToFloat();
 				}
-				else if (element_name == el_posteffect)
+				else if (post_element_name == el_posteffect)
 				{
 					if (m_MapReader.pPostproc)
 						m_MapReader.pPostproc->SetPostEffect(postproc.GetText().FromUTF8());
@@ -710,8 +716,8 @@ void CXMLReader::ReadEnvironment(XMBElement parent)
 				ENSURE(waterbody.GetNodeName() == el_waterbody);
 				XERO_ITER_EL(waterbody, waterelement)
 				{
-					int element_name = waterelement.GetNodeName();
-					if (element_name == el_height)
+					int water_element_name = waterelement.GetNodeName();
+					if (water_element_name == el_height)
 					{
 						CmpPtr<ICmpWaterManager> cmpWaterManager(*m_MapReader.pSimContext, SYSTEM_ENTITY);
 						ENSURE(cmpWaterManager);
@@ -724,7 +730,7 @@ void CXMLReader::ReadEnvironment(XMBElement parent)
 					if (!m_MapReader.pWaterMan)
 						continue;
 
-					if (element_name == el_type)
+					if (water_element_name == el_type)
 					{
 						if (waterelement.GetText() == "default")
 							m_MapReader.pWaterMan->m_WaterType = L"ocean";
@@ -732,18 +738,18 @@ void CXMLReader::ReadEnvironment(XMBElement parent)
 							m_MapReader.pWaterMan->m_WaterType =  waterelement.GetText().FromUTF8();
 					}
 #define READ_COLOR(el, out) \
-					else if (element_name == el) \
+					else if (water_element_name == el) \
 					{ \
-						XMBAttributeList attrs = waterelement.GetAttributes(); \
+						XMBAttributeList colorAttrs = waterelement.GetAttributes(); \
 						out = CColor( \
-							attrs.GetNamedItem(at_r).ToFloat(), \
-							attrs.GetNamedItem(at_g).ToFloat(), \
-							attrs.GetNamedItem(at_b).ToFloat(), \
+							colorAttrs.GetNamedItem(at_r).ToFloat(), \
+							colorAttrs.GetNamedItem(at_g).ToFloat(), \
+							colorAttrs.GetNamedItem(at_b).ToFloat(), \
 							1.f); \
 					}
 
 #define READ_FLOAT(el, out) \
-					else if (element_name == el) \
+					else if (water_element_name == el) \
 					{ \
 						out = waterelement.GetText().ToFloat(); \
 					} \
@@ -981,28 +987,28 @@ int CXMLReader::ReadEntities(XMBElement parent, double end_time)
 			// <position>
 			else if (element_name == el_position)
 			{
-				XMBAttributeList attrs = setting.GetAttributes();
+				XMBAttributeList positionAttrs = setting.GetAttributes();
 				Position = CFixedVector3D(
-					fixed::FromString(attrs.GetNamedItem(at_x)),
-					fixed::FromString(attrs.GetNamedItem(at_y)),
-					fixed::FromString(attrs.GetNamedItem(at_z)));
+					fixed::FromString(positionAttrs.GetNamedItem(at_x)),
+					fixed::FromString(positionAttrs.GetNamedItem(at_y)),
+					fixed::FromString(positionAttrs.GetNamedItem(at_z)));
 			}
 			// <orientation>
 			else if (element_name == el_orientation)
 			{
-				XMBAttributeList attrs = setting.GetAttributes();
+				XMBAttributeList orientationAttrs = setting.GetAttributes();
 				Orientation = CFixedVector3D(
-					fixed::FromString(attrs.GetNamedItem(at_x)),
-					fixed::FromString(attrs.GetNamedItem(at_y)),
-					fixed::FromString(attrs.GetNamedItem(at_z)));
+					fixed::FromString(orientationAttrs.GetNamedItem(at_x)),
+					fixed::FromString(orientationAttrs.GetNamedItem(at_y)),
+					fixed::FromString(orientationAttrs.GetNamedItem(at_z)));
 				// TODO: what happens if some attributes are missing?
 			}
 			// <obstruction>
 			else if (element_name == el_obstruction)
 			{
-				XMBAttributeList attrs = setting.GetAttributes();
-				ControlGroup = attrs.GetNamedItem(at_group).ToInt();
-				ControlGroup2 = attrs.GetNamedItem(at_group2).ToInt();
+				XMBAttributeList obstructionAttrs = setting.GetAttributes();
+				ControlGroup = obstructionAttrs.GetNamedItem(at_group).ToInt();
+				ControlGroup2 = obstructionAttrs.GetNamedItem(at_group2).ToInt();
 			}
 			// <garrison>
 			else if (element_name == el_garrison)
@@ -1011,8 +1017,8 @@ int CXMLReader::ReadEntities(XMBElement parent, double end_time)
 				Garrison.reserve(garrison.size());
 				for (const XMBElement& garr_ent : garrison)
 				{
-					XMBAttributeList attrs = garr_ent.GetAttributes();
-					Garrison.push_back(attrs.GetNamedItem(at_uid).ToInt());
+					XMBAttributeList garrisonAttrs = garr_ent.GetAttributes();
+					Garrison.push_back(garrisonAttrs.GetNamedItem(at_uid).ToInt());
 				}
 			}
 			// <turrets>
@@ -1022,10 +1028,10 @@ int CXMLReader::ReadEntities(XMBElement parent, double end_time)
 				Turrets.reserve(turrets.size());
 				for (const XMBElement& turretPoint : turrets)
 				{
-					XMBAttributeList attrs = turretPoint.GetAttributes();
+					XMBAttributeList turretAttrs = turretPoint.GetAttributes();
 					Turrets.push_back(std::make_pair(
-						attrs.GetNamedItem(at_turret),
-						attrs.GetNamedItem(at_uid).ToInt()
+						turretAttrs.GetNamedItem(at_turret),
+						turretAttrs.GetNamedItem(at_uid).ToInt()
 					));
 				}
 			}

source/graphics/ParticleEmitter.cpp
@@ -1,4 +1,4 @@
-/* Copyright (C) 2015 Wildfire Games.
+/* Copyright (C) 2020 Wildfire Games.
  * This file is part of 0 A.D.
  *
  * 0 A.D. is free software: you can redistribute it and/or modify
@@ -69,7 +69,7 @@ CParticleEmitter::CParticleEmitter(const CParticleEmitterTypePtr& type) :
 	m_IndexArray.SetNumVertices(m_Type->m_MaxParticles * 6);
 	m_IndexArray.Layout();
 	VertexArrayIterator<u16> index = m_IndexArray.GetIterator();
-	for (size_t i = 0; i < m_Type->m_MaxParticles; ++i)
+	for (u16 i = 0; i < m_Type->m_MaxParticles; ++i)
 	{
 		*index++ = i*4 + 0;
 		*index++ = i*4 + 1;

source/graphics/ParticleEmitterType.h
@@ -1,4 +1,4 @@
-/* Copyright (C) 2011 Wildfire Games.
+/* Copyright (C) 2020 Wildfire Games.
  * This file is part of 0 A.D.
  *
  * 0 A.D. is free software: you can redistribute it and/or modify
@@ -100,7 +100,7 @@ private:
 	bool m_UseRelativeVelocity;
 
 	float m_MaxLifetime;
-	size_t m_MaxParticles;
+	u16 m_MaxParticles;
 	CBoundingBoxAligned m_MaxBounds;
 
 	typedef shared_ptr<IParticleVar> IParticleVarPtr;

source/graphics/tests/test_Camera.h
@@ -128,7 +128,7 @@ public:
 
 	void CompareQuads(const CCamera::Quad& quad, const CCamera::Quad& expected_quad)
 	{
-		const float EPS = 1e-4;
+		const float EPS = 1e-4f;
 		for (size_t index = 0; index < expected_quad.size(); ++index)
 		{
 			TS_ASSERT_DELTA(quad[index].X, expected_quad[index].X, EPS);

source/graphics/tests/test_Terrain.h
@@ -451,16 +451,16 @@ public:
 					ss << "Terrain (" << terrain.GetPatchesPerSide()
 					   << "x" << terrain.GetPatchesPerSide() << "):";
 					TS_WARN(ss.str());
-					for (ssize_t jTile = 0; jTile < expectedSize; ++jTile)
+					for (ssize_t jj = 0; jj < expectedSize; ++jj)
 					{
 						ss.str(std::string());
 						ss << "[";
-						for (ssize_t iTile = 0; iTile < expectedSize; ++iTile)
+						for (ssize_t ii = 0; ii < expectedSize; ++ii)
 						{
-							if (iTile)
+							if (ii)
 								ss << ", ";
-							ss << GetVertex(terrain, iTile * PATCH_SIZE,
-							                         jTile * PATCH_SIZE);
+							ss << GetVertex(terrain, ii * PATCH_SIZE,
+							                         jj * PATCH_SIZE);
 						}
 						ss << "]";
 						TS_WARN(ss.str());

source/gui/GUIManager.cpp
@@ -122,7 +122,7 @@ void CGUIManager::PopPage(ScriptInterface::StructuredClone args)
 }
 
 CGUIManager::SGUIPage::SGUIPage(const CStrW& pageName, const ScriptInterface::StructuredClone initData)
-	: name(pageName), initData(initData), inputs(), gui(), callbackFunction()
+	: m_Name(pageName), initData(initData), inputs(), gui(), callbackFunction()
 {
 }
 
@@ -147,7 +147,7 @@ void CGUIManager::SGUIPage::LoadPage(shared_ptr<ScriptContext> scriptContext)
 
 	gui->AddObjectTypes();
 
-	VfsPath path = VfsPath("gui") / name;
+	VfsPath path = VfsPath("gui") / m_Name;
 	inputs.insert(path);
 
 	CXeromyces xero;
@@ -162,7 +162,7 @@ void CGUIManager::SGUIPage::LoadPage(shared_ptr<ScriptContext> scriptContext)
 
 	if (root.GetNodeName() != elmt_page)
 	{
-		LOGERROR("GUI page '%s' must have root element <page>", utf8_from_wstring(name));
+		LOGERROR("GUI page '%s' must have root element <page>", utf8_from_wstring(m_Name));
 		return;
 	}
 
@@ -170,12 +170,12 @@ void CGUIManager::SGUIPage::LoadPage(shared_ptr<ScriptContext> scriptContext)
 	{
 		if (node.GetNodeName() != elmt_include)
 		{
-			LOGERROR("GUI page '%s' must only have <include> elements inside <page>", utf8_from_wstring(name));
+			LOGERROR("GUI page '%s' must only have <include> elements inside <page>", utf8_from_wstring(m_Name));
 			continue;
 		}
 
-		std::string name = node.GetText();
-		CStrW nameW (node.GetText().FromUTF8());
+		CStr8 name = node.GetText();
+		CStrW nameW = node.GetText().FromUTF8();
 
 		PROFILE2("load gui xml");
 		PROFILE2_ATTR("name: %s", name.c_str());
@@ -183,16 +183,16 @@ void CGUIManager::SGUIPage::LoadPage(shared_ptr<ScriptContext> scriptContext)
 		TIMER(nameW.c_str());
 		if (name.back() == '/')
 		{
-			VfsPath directory = VfsPath("gui") / nameW;
-			VfsPaths pathnames;
-			vfs::GetPathnames(g_VFS, directory, L"*.xml", pathnames);
-			for (const VfsPath& path : pathnames)
-				gui->LoadXmlFile(path, inputs);
+			VfsPath currentDirectory = VfsPath("gui") / nameW;
+			VfsPaths directories;
+			vfs::GetPathnames(g_VFS, currentDirectory, L"*.xml", directories);
+			for (const VfsPath& directory : directories)
+				gui->LoadXmlFile(directory, inputs);
 		}
 		else
 		{
-			VfsPath path = VfsPath("gui") / nameW;
-			gui->LoadXmlFile(path, inputs);
+			VfsPath directory = VfsPath("gui") / nameW;
+			gui->LoadXmlFile(directory, inputs);
 		}
 	}
 
@@ -213,7 +213,7 @@ void CGUIManager::SGUIPage::LoadPage(shared_ptr<ScriptContext> scriptContext)
 
 	if (scriptInterface->HasProperty(global, "init") &&
 	    !scriptInterface->CallFunctionVoid(global, "init", initDataVal, hotloadDataVal))
-		LOGERROR("GUI page '%s': Failed to call init() function", utf8_from_wstring(name));
+		LOGERROR("GUI page '%s': Failed to call init() function", utf8_from_wstring(m_Name));
 }
 
 void CGUIManager::SGUIPage::SetCallbackFunction(ScriptInterface& scriptInterface, JS::HandleValue callbackFunc)
@@ -268,7 +268,7 @@ Status CGUIManager::ReloadChangedFile(const VfsPath& path)
 	for (SGUIPage& p : m_PageStack)
 		if (p.inputs.find(path) != p.inputs.end())
 		{
-			LOGMESSAGE("GUI file '%s' changed - reloading page '%s'", path.string8(), utf8_from_wstring(p.name));
+			LOGMESSAGE("GUI file '%s' changed - reloading page '%s'", path.string8(), utf8_from_wstring(p.m_Name));
 			p.LoadPage(m_ScriptContext);
 			// TODO: this can crash if LoadPage runs an init script which modifies the page stack and breaks our iterators
 		}

source/gui/GUIManager.h
@@ -149,7 +149,7 @@ private:
 		 */
 		void PerformCallbackFunction(ScriptInterface::StructuredClone args);
 
-		CStrW name;
+		CStrW m_Name;
 		std::unordered_set<VfsPath> inputs; // for hotloading
 		ScriptInterface::StructuredClone initData; // data to be passed to the init() function
 		shared_ptr<CGUI> gui; // the actual GUI page

commit 811ef651263547b13cc974d56d5e785bde2b2328

Fix: Correct control group iteration in ResolveFoundationCollisions
In `CCmpObstruction::ResolveFoundationCollisions`, the nested loop
intended to assign a persistent control group to colliding normal
entities was incorrectly iterating over `normalEnts` instead of
`persistentEnts`.

This caused the function to attempt to clobber default control groups
with other non-persistent groups, completely bypassing the intended
`ControlPersist` logic.

This patch fixes the inner loop to correctly iterate over
`persistentEnts`, ensuring foundations properly inherit persistent
control groups.

Fixes: 9ae084519f

source/simulation2/components/CCmpObstruction.cpp
@@ -1,4 +1,4 @@
-/* Copyright (C) 2025 Wildfire Games.
+/* Copyright (C) 2026 Wildfire Games.
  * This file is part of 0 A.D.
  *
  * 0 A.D. is free software: you can redistribute it and/or modify
@@ -819,7 +819,7 @@ public:
 			for (const entity_id_t normalEnt : normalEnts)
 			{
 				CmpPtr<ICmpObstruction> cmpObstruction(GetSimContext(), normalEnt);
-				for (const entity_id_t persistent : normalEnts)
+				for (const entity_id_t persistent : persistentEnts)
 				{
 					entity_id_t group = cmpObstruction->GetControlGroup();
 